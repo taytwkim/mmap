faf31860a9023b5b76024d6528bfbf2077c5b1c3
taytwkim <tay.tw.kim@gmail.com>
2026-07-01 14:35:48 -0400

    annotate mmapped file exercise

--- mmap_file.c
+++ mmap_file.c
@@ -7,16 +7,50 @@
 #include <sys/mman.h>
 #include <string.h>
 
-#define FILE_NAME "mmap_test.txt"
+#define FILE_NAME "mmap_file.txt"
 #define FILE_SIZE 8192
 
+/*
+ * What is a memory-mapped file?
+ *
+ * With normal read(), file data is brought into kernel memory,
+ * and then copied into the user-space buffer we provided.
+ *
+ * This means the same file data can exist in two places:
+ * one copy in kernel memory, and another copy in our
+ * user-space buffer.
+ * 
+ * We can index into the buffer using offsets, but we have to be
+ * careful not to read beyond the range that was actually copied
+ * into the buffer.
+ * 
+ * A memory-mapped file can be thought of as dividing a large file
+ * into pages and accessing them like normal memory.
+ *
+ * If we access a part of the mapped file whose page is not currently
+ * in memory, the normal OS page fault mechanism can bring that page in.
+ *
+ * So it is useful to think of a memory-mapped file as treating the file
+ * as a collection of pages that can be randomly accessed.
+ */
+
 int main(void) {
+    /*
+     * Create a file and write contents at specific offsets.
+     * 
+     * O_RDWR   -> open for both reading and writing
+     * O_CREAT  -> create the file if it does not exist
+     * O_TRUNC  -> if it already exists, erase its old contents
+     * 0644     -> permissions for the new file if it is created
+     */
     int fd = open(FILE_NAME, O_RDWR | O_CREAT | O_TRUNC, 0644);
+
     if (fd == -1) {
         perror("open");
         exit(1);
     }
 
+    // Set the file size to FILE_SIZE to make sure its big enough.
     if (ftruncate(fd, FILE_SIZE) == -1) {
         perror("ftruncate");
         exit(1);
@@ -42,7 +76,7 @@ int main(void) {
         NULL,
         FILE_SIZE,
         PROT_READ | PROT_WRITE,
-        MAP_SHARED,
+        MAP_SHARED, // Use MAP_SHARED to modify the actual file.
         fd,
         0
     );
@@ -55,8 +89,11 @@ int main(void) {
     printf("p + 0:    %s", p + 0);
     printf("p + 4096: %s", p + 4096);
 
+    // Modify the file.
+    // Note that we are writing to memory, not using write().
     strcpy(p + 4096, "updated through mmap\n");
 
+    // Ask OS to flush the modified mapped memory back to the actual file now.
     if (msync(p, FILE_SIZE, MS_SYNC) == -1) {
         perror("msync");
         exit(1);
